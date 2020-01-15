7abb83159dcd04894493e9e6917e0d0ba593d0f0
tortolala <tortola@ufm.edu>
2020-01-14 22:50:31 -0600

    Rocket formula added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int mass = 12;
+        int fuel = (mass / 3) - 2;
+        System.out.println("The fuel required is: " + fuel);
+
     }
 }
